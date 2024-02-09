4371cd595aaba66adc350da9d2f5cbfb3e71b99d
Vladimir Shilov <i@vshilov.ru>
2024-02-09 15:46:31 +0300

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Владимир Шилов on 09.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        print(buttonText)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
